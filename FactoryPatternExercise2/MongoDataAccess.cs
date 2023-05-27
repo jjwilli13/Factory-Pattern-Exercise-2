a5bbd58672974eff27facc3f98c9c799b49b1f16
Jordan Williams <jjwilli13@gmail.com>
2023-05-27 06:04:38 -0500

    created interface with classes

---
+++ FactoryPatternExercise2/MongoDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace FactoryPatternExercise2
+{
+    public class MongoDataAccess : IDataAccess
+    {
+        public void LoadData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ FactoryPatternExercise2/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FactoryPatternExercise2
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
+
